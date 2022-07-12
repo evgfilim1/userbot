70396be4c39e7b7ba355a94cef30a1c548395f61
evgfilim1 <evgfilim1@yandex.ru>
2022-07-13 04:08:01 +0500

    Add `.about` command

--- userbot/commands.py
+++ userbot/commands.py
@@ -6,6 +6,7 @@ import random
 import re
 from calendar import TextCalendar
 from datetime import datetime, time, timedelta
+from os import getenv
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
@@ -594,3 +595,19 @@ async def remind_me(client: Client, message: Message, args: str) -> str:
         schedule_date=t,
     )
     return f"⏰ Reminder was set for <i>{t.time()}</i>"
+
+
+@commands.add("about", usage="")
+async def about(_: Client, __: Message, ___: str) -> str:
+    """Shows information about this userbot"""
+    base_url = "https://github.com/evgfilim1/userbot"
+    commit = getenv("GITHUB_SHA", None)
+    # Maybe get this from the git repo, but there's no need for it now
+    t = (
+        f"ℹ️ <b>About userbot</b>\n"
+        f"<i>Repo:</i> <a href='{base_url}'>evgfilim1/userbot</a>\n"
+        f"<i>Commit:</i> <code>{commit or 'staging'}</code>"
+    )
+    if commit:
+        t += f" (<a href='{base_url}/deployments'>deployments</a>)"
+    return t
