5a27cb9bd2078d7c59a3c4940cb9ff28b2d71bb2
evgfilim1 <evgfilim1@yandex.ru>
2022-09-23 20:34:05 +0500

    Send both sticker formats if no args were specified

--- userbot/commands/content_converters.py
+++ userbot/commands/content_converters.py
@@ -3,7 +3,9 @@ __all__ = [
 ]
 
 import asyncio
+from io import BytesIO
 from tempfile import NamedTemporaryFile
+from typing import BinaryIO
 
 from PIL import Image
 from pyrogram import Client
@@ -50,6 +52,16 @@ async def video_to_gif(client: Client, message: Message, __: CommandObject) -> s
         await message.delete()
 
 
+def _convert_to_sticker(photo: BinaryIO, fmt: str) -> BytesIO:
+    img: Image.Image = Image.open(photo)
+    img.thumbnail((512, 512))
+    sticker = BytesIO()
+    sticker.name = f"sticker.{fmt}"
+    img.save(sticker, fmt)
+    sticker.seek(0)
+    return sticker
+
+
 @commands.add(
     "tosticker",
     usage="[reply] ['png'|'webp']",
@@ -58,25 +70,25 @@ async def video_to_gif(client: Client, message: Message, __: CommandObject) -> s
 async def photo_to_sticker(client: Client, message: Message, command: CommandObject) -> None:
     """Converts a photo to a sticker-ready png or webp
 
-    'png' is assumed when no argument is specified."""
+    Both are assumed when no argument is specified."""
     msg = message.reply_to_message if message.reply_to_message else message
-    output_io = await client.download_media(msg, in_memory=True)
-    output_io.seek(0)
-    im: Image.Image = Image.open(output_io)
-    im.thumbnail((512, 512))
-    output_io.seek(0)
-    fmt = command.args.lower() if command.args else "png"
-    if fmt not in ("png", "webp"):
-        raise ValueError(f"Unsupported format: {fmt}")
-    im.save(output_io, fmt)
-    output_io.seek(0)
-    output_io.name = f"sticker.{fmt}"
-    match fmt:
-        case "png":
-            await msg.reply_document(output_io, file_name="sticker.png")
-        case "webp":
-            await msg.reply_sticker(output_io)
-        case _:
-            raise AssertionError("Wrong format, this should never happen")
+    image = await client.download_media(msg, in_memory=True)
+    image.seek(0)
+    args = command.args.lower()
+    if not args:
+        fmts = ("png", "webp")
+    else:
+        if args not in ("png", "webp"):
+            raise ValueError(f"Unsupported format: {args}")
+        fmts = (args,)
+    for fmt in fmts:
+        sticker = _convert_to_sticker(image, fmt)
+        match fmt:
+            case "png":
+                await msg.reply_document(sticker, file_name="sticker.png")
+            case "webp":
+                await msg.reply_sticker(sticker)
+            case _:
+                raise AssertionError("Wrong format, this should never happen")
     if message.reply_to_message:
         await message.delete()
