84e59fccee5f0d26accac407734cd00b31410781
evgfilim1 <evgfilim1@yandex.ru>
2022-09-30 21:14:08 +0500

    Add Crowdin link to `.about`

--- userbot/commands/about.py
+++ userbot/commands/about.py
@@ -3,38 +3,42 @@ __all__ = [
 ]
 
 from os import getenv
+from typing import Type
 
 from pyrogram import Client
 
-from ..constants import DefaultIcons, PremiumIcons
+from ..constants import Icons, PremiumIcons
 from ..modules import CommandsModule
 from ..translation import Translation
+from ..utils import is_prod
 
 commands = CommandsModule("About")
 
 
 @commands.add("about")
-async def about(client: Client, tr: Translation) -> str:
+async def about(client: Client, icons: Type[Icons], tr: Translation) -> str:
     """Shows information about this userbot"""
     _ = tr.gettext
     base_url = "https://github.com/evgfilim1/userbot"
     # TODO (2022-07-13): get this from the git repo also
-    commit = getenv("GITHUB_SHA", None)
+    commit = getenv("GITHUB_SHA", _("staging"))
     if client.me.is_premium:
-        header_icon = PremiumIcons.INFO
         github_icon = PremiumIcons.GITHUB
         commit_icon = PremiumIcons.GIT
     else:
-        header_icon = DefaultIcons.INFO
         github_icon = _("<i>Repo:</i>")
         commit_icon = _("<i>Commit:</i>")
-    t = _("{header_icon} <b>About userbot</b>\n").format(header_icon=header_icon)
+    t = _("{icon} <b>About userbot</b>\n").format(icon=icons.INFO)
     t += (
         f"{github_icon} <a href='{base_url}'>evgfilim1/userbot</a>\n"
-        f"{commit_icon} <code>{commit or 'staging'}</code>"
+        f"{commit_icon} <code>{commit}</code>"
     )
-    if commit:
+    if is_prod():
         t += _(" (<a href='{base_url}/deployments'>deployments</a>)").format(
             base_url=base_url,
         )
+    t += _("\n{icon} <a href='{url}'>Contribute userbot translations</a>").format(
+        icon=icons.GLOBE,
+        url="https://crowdin.com/project/evgfilim1-userbot",
+    )
     return t
